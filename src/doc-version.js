b075d8e62c706da3db5becaad235cd942e50dead
kepeket <kprettre@gmail.com>
2016-09-30 14:59:03 +0200

    Move click event binding from index.js to DocVersion
    Use the same behavior as the DocSquare

--- src/doc-version.js
+++ src/doc-version.js
@@ -3,6 +3,31 @@
  */
 
 var DocVersion = React.createClass({
+    handleClick: function(path){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        newSrc = path;
+        $(this).closest('.doc-version').siblings().removeClass('active');
+        $(this).closest('.doc-version').addClass('active');
+        oldSrc = $('#docframe')[0].contentWindow.location.href;
+        $('#frame iframe').attr('src', newSrc);
+        $('#frame iframe').on('load', function(e){
+            $('#index, #frame').addClass('open');
+            iframebody = e.target.contentWindow.document.body;
+            h = $(iframebody).find('.rst-content').height()+100;
+            // albater theme support
+            if (h == 100){
+                h = $(iframebody).find('.document').height()+100;
+            }
+            // jsdoc support
+            if (h == 100){
+                h = $(iframebody).find('#main').height()+100;
+            }
+            $('#frame').height(h+'px');
+            $('#new-window').attr('href', e.target.contentWindow.location.href);
+        });
+    },
     render: function() {
     	var divStyle = {
             backgroundColor: this.props.color
@@ -11,10 +36,10 @@ var DocVersion = React.createClass({
         if (this.props.version != 'master'){
             additionalClass = 'tag';
         }
+        var boundClick = this.handleClick.bind(this, this.props.url);
         return (
-            <div className={"doc-square doc-version " + additionalClass} style={divStyle} >
+            <div className={"doc-square doc-version " + additionalClass} style={divStyle} onClick={boundClick}>
                 <h3>{this.props.name}</h3>
-                <a href={this.props.url}>browse</a>
             </div>
         )
     },
--- src/index.js
+++ src/index.js
@@ -3,45 +3,17 @@
   */
 
 var DocBox = require('./doc-box');
-var ToolBox = require('./tool-box');
+//var ToolBox = require('./tool-box');
 
 ReactDOM.render(
     <div className="main">
         <h1>LBC Tech doc</h1>
-        <ToolBox />
-        <DocBox pollInterval={20000}/>
+        <DocBox pollInterval={10000}/>
     </div>,
     document.getElementById('index')
 );
 
 $(document).ready(function(){
-	$('#index').on('click', '.doc-version a', function(e){
-		e.preventDefault();
-        $('html, body').animate({
-            scrollTop: 0
-        }, 500);
-		newSrc = $(e.target).attr('href');
-		$(e.target).closest('.doc-version').siblings().removeClass('active');
-		$(e.target).closest('.doc-version').addClass('active');
-		oldSrc = $('#docframe')[0].contentWindow.location.href;
-		$('#frame iframe').attr('src', $(e.target).attr('href'));
-		$('#frame iframe').on('load', function(e){
-			$('#index, #frame').addClass('open');
-		        iframebody = e.target.contentWindow.document.body;
-                    h = $(iframebody).find('.rst-content').height()+100;
-                    // albater theme support
-                    if (h == 100){
-                        h = $(iframebody).find('.document').height()+100;
-                    }
-                    // jsdoc support
-                    if (h == 100){
-                        h = $(iframebody).find('#main').height()+100;
-                    }
-			$('#frame').height(h+'px');
-			$('#new-window').attr('href', e.target.contentWindow.location.href);
-		});
-	});
-
 	$('#close-panel').click(function(){
 		$('.doc-version').removeClass('active');
 		$('#index, #frame').removeClass('open');
